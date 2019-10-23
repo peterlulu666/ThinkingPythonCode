4f50772fcb8b6253a6d8afad0561aa975dda04f2
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-10-23 08:37:32 -0400

    commit

--- Digits.py
+++ Digits.py
@@ -1,5 +1,11 @@
 def main():
     number = int(input("Enter a integer \n"))
+    # number // 1000 is the 1000-units
+    # if the number was greater than 10000,
+    # we would use % 10 to find leftover
+    thousands = (number // 1000) % 10
+
+    print("hundreds = ", thousands)
     # number // 100 is the 100-units
     # if the number was greater than 1000,
     # we would use % 10 to find leftover
