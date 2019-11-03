1bc1795481bbe27d34d9ae5cd6753a94abaffae9
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-11-03 10:18:06 -0500

    commit

--- racepace.py
+++ racepace.py
@@ -2,40 +2,47 @@ def main():
     SECONDS_PER_HOUR = 3600
     SECONDS_PER_MINUTE = 60
     # Prompt the user for the number of kilometers, hours, and minutes
-    # Number of kilometers they ran, a floating-point number
     while True:
-        # make sure that the user has provided an integer, not a float or a string or anything else
-        # isdigit() checks whether the string consists of digits only
-        try:
-            kilometers = float(input("How many kilometers did you run? \n"))
-            if kilometers >= 0:
-                break
-            else:
-                print("Oops, that wasn't a positive number, please try again!")
-        except ValueError:
-            print("Oops, that wasn't a floating-point number, please try again!")
-    # Number of hours, a whole number
-    hours = input("How many hours did it take you? \n")
-    while True:
-        # make sure that the user has provided an integer, not a float or a string or anything else
-        # isdigit() checks whether the string consists of digits only
-        if not hours.isdigit():
-            print("Oops, that wasn't a whole number, please try again!")
-        else:
-            hours = int(hours)
-            break
+        # Number of kilometers they ran, a floating-point number
+        while True:
+            # make sure that the user has provided an integer, not a float or a string or anything else
+            # isdigit() checks whether the string consists of digits only
+            try:
+                kilometers = float(input("How many kilometers did you run? \n"))
+                if kilometers >= 0:
+                    break
+                else:
+                    print("Oops, that wasn't a positive number, please try again!")
+            except ValueError:
+                print("Oops, that wasn't a floating-point number, please try again!")
+        # Number of hours, a whole number
         hours = input("How many hours did it take you? \n")
-    # Number of minutes, a whole number
-    minutes = input("How many minutes? \n")
-    while True:
-        # make sure that the user has provided an integer, not a float or a string or anything else
-        # isdigit() checks whether the string consists of digits only
-        if not minutes.isdigit():
-            print("Oops, that wasn't a whole number, please try again!")
-        else:
-            minutes = int(minutes)
-            break
+        while True:
+            # make sure that the user has provided an integer, not a float or a string or anything else
+            # isdigit() checks whether the string consists of digits only
+            if not hours.isdigit():
+                print("Oops, that wasn't a whole number, please try again!")
+            else:
+                hours = int(hours)
+                break
+            hours = input("How many hours did it take you? \n")
+        # Number of minutes, a whole number
         minutes = input("How many minutes? \n")
+        while True:
+            # make sure that the user has provided an integer, not a float or a string or anything else
+            # isdigit() checks whether the string consists of digits only
+            if not minutes.isdigit():
+                print("Oops, that wasn't a whole number, please try again!")
+            else:
+                minutes = int(minutes)
+                break
+            minutes = input("How many minutes? \n")
+        # Hours and minutes are both whole numbers,and either one may be zero, but the ​total​ time is never zero
+        if hours == 0 and minutes == 0:
+            print("The ​total​ time can not both be zero \n")
+            continue
+        else:
+            break
     # Compute the total number of miles ran, rounded off to two decimal places
     miles = round(0.621371 * kilometers, 2)
     # Compute the minutes and seconds pace-per-mile (​head’s up!!!​ If you do some calculation and
